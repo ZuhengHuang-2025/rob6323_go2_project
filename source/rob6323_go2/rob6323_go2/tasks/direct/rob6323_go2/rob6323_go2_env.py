4ffcf3d3051c2ffdaea3ada7db15355b849036cb
Zuheng Huang <zh3187@nyu.edu>
2025-12-07 22:57:34 -0500

    Implement Reward Terms

--- source/rob6323_go2/rob6323_go2/tasks/direct/rob6323_go2/rob6323_go2_env.py
+++ source/rob6323_go2/rob6323_go2/tasks/direct/rob6323_go2/rob6323_go2_env.py
@@ -47,6 +47,10 @@ class Rob6323Go2Env(DirectRLEnv):
                 "track_ang_vel_z_exp",
                 "rew_action_rate",     # <--- Added
                 "raibert_heuristic"    # <--- Added
+                "orient",
+                "lin_vel_z",
+                "dof_vel",
+                "ang_vel_xy",
             ]
         }
         self.last_actions = torch.zeros(self.num_envs, gym.spaces.flatdim(self.single_action_space), 3, dtype=torch.float, device=self.device, requires_grad=False)
@@ -161,11 +165,30 @@ class Rob6323Go2Env(DirectRLEnv):
         rew_action_rate += torch.sum(torch.square(self._actions - 2 * self.last_actions[:, :, 0] + self.last_actions[:, :, 1]), dim=1) * (self.cfg.action_scale ** 2)
         
         rew_raibert_heuristic = self._reward_raibert_heuristic()
+        
+        # 1. Penalize non-vertical orientation (projected gravity on XY plane)
+        # Gravity vector should be [0, 0, -1]. Projected gravity is in body frame.
+        rew_orient = torch.sum(torch.square(self.robot.data.projected_gravity_b[:, :2]), dim=1)
+        # 2. Penalize vertical velocity (z-component of base linear velocity)
+        # Penalize bouncing or drift in Z axis.
+        rew_lin_vel_z = torch.square(self.robot.data.root_lin_vel_b[:, 2])
+        # 3. Penalize high joint velocities
+        # Penalize energy consumption/fast movements.
+        rew_dof_vel = torch.sum(torch.square(self.robot.data.joint_vel), dim=1)
+        # 4. Penalize angular velocity in XY plane (roll/pitch)
+        # Penalize wobbling (roll) or pitching forward/back too fast.
+        rew_ang_vel_xy = torch.sum(torch.square(self.robot.data.root_ang_vel_b[:, :2]), dim=1)
+
         rewards = {
             "track_lin_vel_xy_exp": lin_vel_error_mapped * self.cfg.lin_vel_reward_scale, # Removed step_dt
             "track_ang_vel_z_exp": yaw_rate_error_mapped * self.cfg.yaw_rate_reward_scale, # Removed step_dt
             "rew_action_rate": rew_action_rate * self.cfg.action_rate_reward_scale,
             "raibert_heuristic": rew_raibert_heuristic * self.cfg.raibert_heuristic_reward_scale,
+            # --- NEW REWARDS ADDED TO DICTIONARY ---
+            "orient": rew_orient * self.cfg.orient_reward_scale,
+            "lin_vel_z": rew_lin_vel_z * self.cfg.lin_vel_z_reward_scale,
+            "dof_vel": rew_dof_vel * self.cfg.dof_vel_reward_scale,
+            "ang_vel_xy": rew_ang_vel_xy * self.cfg.ang_vel_xy_reward_scale,
         }
         reward = torch.sum(torch.stack(list(rewards.values())), dim=0)
         # Logging
